faa81b7d8c65a3946fd0d9240d348cf143aa86c0
Slad3 <feba23@live.com>
2025-09-16 15:49:51 -0500

    Adding Misc section

--- src/analysis/CSICategoryAnalysis.py
+++ src/analysis/CSICategoryAnalysis.py
@@ -25,6 +25,15 @@ def to_json_grouped_by_month(df: pl.DataFrame) -> dict:
     return result
 
 
+def to_json_grouped_by_year(df: pl.DataFrame) -> dict:
+    result = {}
+    for (year,), group in df.group_by("year"):
+        columns = df.columns
+        columns.remove("year")
+        result[year] = group.select(columns).to_dicts()
+    return result
+
+
 def to_json_grouped_by_category(df: pl.DataFrame) -> dict:
     result = {}
     for (year,), group in df.group_by("categoryid"):
@@ -50,11 +59,7 @@ def get_csi_products_df(df: pl.DataFrame):
 
 def get_csi_category_monthly_counts(df: pl.DataFrame):
     return (
-        df.with_columns(
-            pl.col("changedate").str.strptime(
-                pl.Datetime, strict=False
-            )  # convert str → datetime
-        )
+        df.with_columns(pl.col("changedate").str.strptime(pl.Datetime, strict=False))
         .with_columns(pl.col("changedate").dt.strftime("%Y-%m").alias("yearMonth"))
         .group_by(["yearMonth", "categoryid"])
         .agg(pl.col("cost").count().alias("totalCount"))
@@ -88,6 +93,93 @@ def get_csi_category_monthly_revenues(df: pl.DataFrame):
     )
 
 
+def get_csi_category_yearly_counts(df: pl.DataFrame):
+    return (
+        df.with_columns(pl.col("changedate").str.strptime(pl.Datetime, strict=False))
+        .with_columns(pl.col("changedate").dt.strftime("%Y").alias("year"))
+        .group_by(["year", "categoryid"])
+        .agg(pl.col("cost").count().alias("totalCount"))
+        .with_columns(
+            (
+                pl.col("totalCount") / pl.col("totalCount").sum().over("year") * 100
+            ).alias("percentage")
+        )
+        .sort(["year", "categoryid"])
+        .select("year", "categoryid", "totalCount", "percentage")
+    )
+
+
+def get_csi_category_yearly_revenues(df: pl.DataFrame):
+    return (
+        df.with_columns(pl.col("changedate").str.strptime(pl.Datetime, strict=False))
+        .with_columns(pl.col("changedate").dt.strftime("%Y").alias("year"))
+        .group_by(["year", "categoryid"])
+        .agg(pl.col("cost").sum().alias("totalRevenue"))
+        .with_columns(
+            (
+                pl.col("totalRevenue") / pl.col("totalRevenue").sum().over("year") * 100
+            ).alias("percentage")
+        )
+        .sort(["year", "categoryid"])
+        .select("year", "categoryid", "totalRevenue", "percentage")
+    )
+
+
+def get_csi_category_total_counts(df: pl.DataFrame):
+    return (
+        df.with_columns(pl.col("changedate").str.strptime(pl.Datetime, strict=False))
+        .group_by(["categoryid"])
+        .agg(pl.col("cost").count().alias("totalCount"))
+        .with_columns(
+            (
+                pl.col("totalCount") / pl.col("totalCount").sum() * 100
+            ).alias("percentage")
+        )
+        .sort(["categoryid"])
+        .select("categoryid", "totalCount", "percentage")
+    )
+
+
+def get_csi_category_total_revenues(df: pl.DataFrame):
+    return (
+        df.with_columns(pl.col("changedate").str.strptime(pl.Datetime, strict=False))
+        .group_by(["categoryid"])
+        .agg(pl.col("cost").sum().alias("totalRevenue"))
+        .with_columns(
+            (
+                pl.col("totalRevenue") / pl.col("totalRevenue").sum() * 100
+            ).alias("percentage")
+        )
+        .sort(["categoryid"])
+        .select("categoryid", "totalRevenue", "percentage")
+    )
+
+
+def get_mics_totals(df: pl.DataFrame):
+    return (
+        df.filter(
+            (pl.col("partnum").str.to_lowercase().str.contains("ship"))
+            | (pl.col("partnum").str.to_lowercase().str.contains("sys"))
+            | (pl.col("partnum").str.to_lowercase().str.contains("hdw"))
+        )
+        .with_columns(
+            [
+                pl.when(pl.col("partnum").str.to_lowercase().str.contains("ship"))
+                .then(pl.lit("ship"))
+                .when(pl.col("partnum").str.to_lowercase().str.contains("sys"))
+                .then(pl.lit("sys"))
+                .when(pl.col("partnum").str.to_lowercase().str.contains("hdw"))
+                .then(pl.lit("hdw"))
+                .alias("category")
+            ]
+        )
+        .group_by("category")
+        .agg([pl.len().alias("count"), pl.sum("unitprice").alias("revenue")])
+    )
+
+
+
+
 def csi_category_overview_analysis(df: pl.DataFrame):
     csi_products_df = get_csi_products_df(df=df).filter(
         pl.col("categoryid").is_in(
@@ -102,6 +194,23 @@ def csi_category_overview_analysis(df: pl.DataFrame):
     )
 
     result = {"overview": {}}
+
+    result['overview']['misc'] = get_mics_totals(df=df).to_dicts()
+
+    result["overview"]["categoryTotalCounts"] = to_json_grouped_by_category(
+        get_csi_category_total_counts(csi_products_df)
+    )
+    result["overview"]["categoryTotalRevenues"] = to_json_grouped_by_category(
+        get_csi_category_total_revenues(csi_products_df)
+    )
+
+
+    result["overview"]["categoryYearlyCounts"] = to_json_grouped_by_year(
+        get_csi_category_yearly_counts(csi_products_df)
+    )
+    result["overview"]["categoryYearlyRevenues"] = to_json_grouped_by_year(
+        get_csi_category_yearly_revenues(csi_products_df)
+    )
 
     csi_category_montly_counts = get_csi_category_monthly_counts(csi_products_df)
     csi_category_montly_revenue = get_csi_category_monthly_revenues(csi_products_df)
